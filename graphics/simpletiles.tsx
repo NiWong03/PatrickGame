f31fb057733e52d5ac9fb6d3381f116c2f3166e0
Nicholas Wong <Nickstorm07@gmail.com>
2024-11-03 01:27:15 -0700

    added files

---
+++ graphics/simpletiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tileset_version1.1" tilewidth="128" tileheight="128" tilecount="8" columns="4">
+ <image source="simple_tileset_32x32_version1.1/tileset_version1.1.png" width="512" height="352"/>
+</tileset>
